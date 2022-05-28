e3d9d5c07db0dfe0cebd5a4f9bbe17a9a0c73a69
Jang.hyunjeong <jang.hyunjeong01@gmail.com>
2022-05-28 13:14:28 +0900

    ✨ write 완료 시, data array 추가

--- src/routes/WritePage.js
+++ src/routes/WritePage.js
@@ -2,22 +2,48 @@ import './write-page.css';
 import { useState } from 'react';
 
 function WritePage() {
-  const [content, setContent] = useState("");
-  const onChange = (event) => setContent(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [dataArray, setDataArray] = useState([]);
+  const key = dataArray.length;
+
+  const getTitle = (event) => {
+    setTitle(event.target.value);
+    //console.log(title);
   }
+  const getContent = (event) => {
+    setContent(event.target.value);
+  }
+
+  //object형태로 받아오기
+  const data = {
+    key: key,
+    title: title,
+    content: content
+  }
+
+  const getData = (event) => {
+    event.preventDefault();
+    setDataArray([...dataArray, data]);
+  }
+
   return (
     <div className="write-page">
       <div className='sub-inner'>
         <h2 className='sub-title'>작성하기</h2>
         <form>
+          <input 
+            type="text"
+            placeholder="제목을 입력해주세요."
+            value={title}
+            onChange={getTitle}
+          >
+          </input>
           <textarea
             value={content}
-            onChange={onChange}
             placeholder="내용을 입력해주세요."
-          >
-            
+            onChange={getContent}
+          >            
           </textarea>
 
           <div className='button-wrap'>
@@ -28,15 +54,10 @@ function WritePage() {
             </button>
             <button 
             type='submit'
-            onClick={onSubmit}
             className='button button-ok'
+            onClick={getData}
             >확인
             </button>
-          </div>
-
-
-          <div className='view-page'>
-            {content}
           </div>
         </form>
       </div>
