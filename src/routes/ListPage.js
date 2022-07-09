6250e94c847c86de06ffd7f392c1adb2b5a9f028
Jang.hyunjeong <jang.hyunjeong01@gmail.com>
2022-07-09 13:14:00 +0900

    jhj 데이터 삭제

--- src/routes/ListPage.js
+++ src/routes/ListPage.js
@@ -1,24 +1,59 @@
 import * as React from 'react';
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
 
 function ListPage() {
     const contentArray =  window.localStorage.getItem("contentArray");
-    const parsedData = JSON.parse(contentArray);
-    console.log(parsedData);
+    
+    // 컴포넌트가 바로 업데이트 될 수있도록 useSate를 사용해, props 를 업데이트해준다.
+    const [parsedData, setParsedData] = useState(JSON.parse(contentArray));
+
+    // 삭제 function
+    const actionDel = (e, key) => {
+      e.preventDefault();
+      //const newArray = parsedData.filter((item) => item.key !== key);
+
+      // item.key = key인것을 제외하고 필터링 (새로운 배얼을 만듦)]
+
+     //결과적으로 수정해야하는 값을 수정해여야해.
+     //localStorage에 있는 값을 변환하고싶은거니까, 결과적으로 그값을 수정하고, 
+      window.localStorage.setItem("contentArray", JSON.stringify(parsedData.filter((item) => item.key !== key)));
+
+      //데이터는 그 다음에 set해주면 돼. 먼저 set 하면 오류나!!!
+      setParsedData(JSON.parse(window.localStorage.getItem("contentArray")));
+      
+    }
       return (
         <div className="list-page">
           <div className='sub-inner'>
             <h2 className='sub-title'>리스트 페이지</h2>
             <form>
               <ul className="list-content">
+                <li className="list-header">
+                  <div>번호</div>
+                  <div>제목</div>
+                  <div>관리</div>
+                </li>
                   {parsedData != null ? (
                     parsedData.map((item, index) => 
-                    <li key={item.key}>
-                      {item.value}
+                    <li className="list-body" key={item.key}>
+                      <div>{item.key}</div>
+                      <div className="subject">{item.value}</div>
+                      <div>
+                        <button 
+                          type="button" 
+                          className="button-del"
+                          onClick={(e) =>     {actionDel(e, item.key)}
+                          }
+                        >
+                          {/* 01. 클릭한 애의 키값 받아오기 */}
+                          {/* 02. 키값이 지금숫자인 애를 배열에서 필터링*/}
+                          삭제</button>
+                      </div>
                     </li>
                     )
                   ) : (
-                    <li>등록된 게시글 없습니다.</li>
+                    <li className="list-empty">등록된 게시글 없습니다.</li>
                   )}
               </ul>
     
